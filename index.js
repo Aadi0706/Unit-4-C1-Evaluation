cbea364d5fcf932bb069755af01c4ec4e6255925
Aadi0706 <97503774+Aadi0706@users.noreply.github.com>
2022-03-07 12:50:06 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const express= require('express');
+const app = express();
+
+app.get("/books",logger,(req,res)=>{
+    return res.send({route:req.role});
+});
+
+app.get("/libraries",logger,checkPermission,(req,res)=>{
+    return res.send({route:req.role,permission:req.permission});
+})
+
+app.get("/authors",logger,checkPermission,(req,res)=>{
+    return res.send({route:req.role,permission:req.permission});
+})
+function logger(req,res,next){
+        // console.log("logger before response send");
+        if(req.path=="/books"){
+            req.role="/books";
+        }
+        if(req.path=="/libraries"){
+            req.role="/libraries";
+        }
+        if(req.path=="/authors"){
+            req.role="/authors";
+        }
+        next();
+}
+
+function checkPermission(req,res,next)
+{
+      if(req.path=="/libraries"){
+         req.permission="true";
+      }
+      if(req.path=="/authors"){
+       req.permission="true";
+      }
+      next();
+}
+
+
+
+app.listen(5500,()=>{
+    console.log("listening on port 5500");
+})
